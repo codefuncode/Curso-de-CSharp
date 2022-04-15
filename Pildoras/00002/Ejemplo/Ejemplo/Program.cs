c81c1ee662f378a2fb32a27961d53c7d8d6df3f9
Carlos Aleman <caleman9791@gmail.com>
2022-04-15 17:33:14 -0400

    Primera aplicación de consola.

---
+++ Pildoras/00002/Ejemplo/Ejemplo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo{
+    internal class Program{
+
+        static void Main(string[] args){
+            System.Console.WriteLine("Primera palicacion de cponsola en C#");
+            System.Console.WriteLine("=========================C#");
+            Console.WriteLine("Primera palicacion de cponsola en C#");
+
+        }
+    }
+}
