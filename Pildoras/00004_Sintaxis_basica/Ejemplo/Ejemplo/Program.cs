4bb6f670a6d5643ac8433122e8ed0fc127b53195
Carlos Aleman <caleman9791@gmail.com>
2022-04-15 18:56:27 -0400

    Modulo 4  declaracion de variables

---
+++ Pildoras/00004_Sintaxis_basica/Ejemplo/Ejemplo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo
+{
+   internal class Program{
+       static void Main(string[] args){
+
+       int myNum = 5;
+       double myDoubleNum = 5.99D;
+       char myLetter = 'D';
+       bool myBool = true;
+       string myText = "Hello";
+
+       Console.WriteLine(myNum);
+       Console.WriteLine(myDoubleNum);
+       Console.WriteLine(myLetter);
+       Console.WriteLine(myBool);
+       Console.WriteLine(myText);
+
+            byte edad = 28;
+            Console.WriteLine(edad);
+       }
+   }
+}
