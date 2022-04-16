f4eeb6907a3b5536a7c058f8e77e9d9c1db71f1a
Carlos Aleman <caleman9791@gmail.com>
2022-04-15 23:05:24 -0400

    Modulo 5 sintaxis basica uso de operadores

--- Pildoras/00005_Sintaxis_basica/Ejemplo/Ejemplo/Program.cs
+++ Pildoras/00005_Sintaxis_basica/Ejemplo/Ejemplo/Program.cs
@@ -9,7 +9,47 @@ namespace Ejemplo{
     internal class Program{
         
         static void Main(string[] args){
-            
+
+            Console.WriteLine(7 * 5); 
+
+            Console.WriteLine("======================================"); 
+            // ////////////////////////////////////////////////////////
+            // Esto dará como resultado un valor de tipo int 
+            Console.WriteLine(5/2);
+            //  Imprime 
+            // ////////////////////////////////////////////////////////
+
+            Console.WriteLine("======================================"); 
+
+            // ///////////////////////////////////////////////////////
+            //  Dará como resultado un valor decimal
+            Console.WriteLine(5.0/2.0);
+            // ///////////////////////////////////////////////////////
+
+            Console.WriteLine("======================================"); 
+
+            // ///////////////////////////////////////////////////////
+            //  Dará como resultado un valor decimal
+            Console.WriteLine(9 % 3);
+            // Imprime 0
+            Console.WriteLine("======================================"); 
+            // ///////////////////////////////////////////////////////
+
+            // ///////////////////////////////////////////////////////
+            //  Dará como resultado un valor decimal
+            Console.WriteLine(9 % 4);
+            // Imprime 1
+            Console.WriteLine("======================================");
+            // ///////////////////////////////////////////////////////
+
+
+            int edad = 40;
+            // edad++;
+            Console.WriteLine($"Tienes una edad de {++edad} años");  
+            Console.WriteLine($"Tienes una edad de {edad++} años");  
+            Console.WriteLine($"Tienes una edad de {edad} años");  
+            Console.WriteLine($"Tienes una edad de {edad} años");
+         
         }
     }
 }
