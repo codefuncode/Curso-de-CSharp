f4234663da84d8dd769f0367499ecbabb6fad5c9
Carlos Aleman <caleman9791@gmail.com>
2022-04-15 23:06:55 -0400

    Modulo 6 detalles importantes sobre la declaracion de variables faltan comentarios para el proximo commmit ;wq

---
+++ Pildoras/00006_Sintaxis_basica/Modulo00006/Modulo00006/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulo00006
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ok");
+            int edadPersona = 40;
+            int edadPersona1;
+            int edadPersona2;
+            int edadPersona3;
+            int edadPersona4;
+            int edadPersona5;
+
+
+            edadPersona1 =
+            edadPersona2 =
+            edadPersona3 =
+            edadPersona4 =
+            edadPersona5 = edadPersona;
+
+            // int edadPersona = 40;
+            Console.WriteLine(edadPersona1);
+            Console.WriteLine(edadPersona2);
+            Console.WriteLine(edadPersona3);
+            Console.WriteLine(edadPersona4);
+            Console.WriteLine(edadPersona5);
+
+        }
+    }
+}
