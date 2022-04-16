a5a51b6ec44511a59085f99cf82c9c6ec8195f7e
Carlos Aleman <caleman9791@gmail.com>
2022-04-16 14:37:47 -0400

    Generado por VS

--- Pildoras/00005_Sintaxis_basica/Ejemplo/Ejemplo/Program.cs
+++ Pildoras/00005_Sintaxis_basica/Ejemplo/Ejemplo/Program.cs
@@ -11,7 +11,9 @@ namespace Ejemplo{
         static void Main(string[] args){
 
             Console.WriteLine(7 * 5); 
-
+            ////////////////////////////////////////////////////////////////////////////////////////////
+            // Nota en la división si los valores ambos son enteros el resultado 
+            // será un entero y si hay decimal el resultado será decimal
             Console.WriteLine("======================================"); 
             // ////////////////////////////////////////////////////////
             // Esto dará como resultado un valor de tipo int 
@@ -25,7 +27,7 @@ namespace Ejemplo{
             //  Dará como resultado un valor decimal
             Console.WriteLine(5.0/2.0);
             // ///////////////////////////////////////////////////////
-
+            ////////////////////////////////////////////////////////////////////////////////////////////
             Console.WriteLine("======================================"); 
 
             // ///////////////////////////////////////////////////////
@@ -45,10 +47,44 @@ namespace Ejemplo{
 
             int edad = 40;
             // edad++;
-            Console.WriteLine($"Tienes una edad de {++edad} años");  
+            // -------------------------------------------------------
+            // Operador ++ al principio adjudica valor y despliega 
+            Console.WriteLine($"Tienes una edad de {++edad} años"); 
+            // -------------------------------------------------------
+            // Operador ++ a continuación despliega valor y después adjudica 
             Console.WriteLine($"Tienes una edad de {edad++} años");  
+            // -----------------------------------------------------
+
             Console.WriteLine($"Tienes una edad de {edad} años");  
             Console.WriteLine($"Tienes una edad de {edad} años");
+
+            var jh = (
+                firstName: "Jupiter",
+                 lastName: "Hammon",
+                  born: 1711,
+                   published: 1761
+                );
+            // //////////////////////////////////////////////////
+            //  Note la posibilidad de continuar en una nueva linea 
+            //  usando operador mas y signo de $ en la linea a continuación 
+            Console.WriteLine(
+                $"{jh.firstName} " +
+                $"{jh.lastName} " +
+                $"fue un poeta afroamericano nacido en " +
+                $"{jh.born}."
+                );
+            // //////////////////////////////////////////////////
+            Console.WriteLine(
+                $"Se publicó por primera vez en {jh.published}  a la edad de {jh.published - jh.born} años."
+                );
+            Console.WriteLine(
+                $"Tendría más de {Math.Round((2018d - jh.born) / 100d) * 100d} años hoy."
+                );
+
+            // Producción:
+            // Jupiter Hammon fue un poeta afroamericano nacido en 1711.
+            // Se publicó por primera vez en 1761 a la edad de 50 años.
+            // Tendría más de 300 años hoy.
          
         }
     }
