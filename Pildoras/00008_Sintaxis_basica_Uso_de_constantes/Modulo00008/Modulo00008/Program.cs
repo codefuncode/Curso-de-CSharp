2c3723d5f51979a5196072897d2473ec2f42eaeb
Carlos Aleman <caleman9791@gmail.com>
2022-04-19 23:39:51 -0400

    Modulo 8

---
+++ Pildoras/00008_Sintaxis_basica_Uso_de_constantes/Modulo00008/Modulo00008/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulo00008{
+
+    internal class Program{
+
+        static void Main(string[] args){
+
+            //  Las constantes deben declarar e iniciar en la misma linea 
+
+            // const int VALOR = 20; 
+            // const int VALOR2 = 21;
+
+            //  No se permite cambiar el valor de las constantes 
+            // VALOR = 21;
+
+            // Console.WriteLine("El valor de la constante es {1}",VALOR,VALOR2);
+
+
+            Console.WriteLine("Introduce el radio para calcular el área del circulo");
+            const double PI = 3.1416;
+
+            double radio = double.Parse(Console.ReadLine());
+
+            double area = radio * radio * PI;
+
+            Console.WriteLine($"El área del; circulo es {area}");
+
+
+        }
+    }
+}
