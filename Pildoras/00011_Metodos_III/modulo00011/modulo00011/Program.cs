0b3d59afa939d3c7bae40aab18ee6b26baef783c
Carlos Aleman <caleman9791@gmail.com>
2022-04-27 02:50:26 -0400

    Agregar : Módulo 11
    Contiene algunos ejemplos de sintaxis básica de C#
    
    Carlos Alemán
    @caleman9791

---
+++ Pildoras/00011_Metodos_III/modulo00011/modulo00011/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modulo00011 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int myNum1 = PlusMethod(8, 5);
+            double myNum2 = PlusMethod(4.3, 6.26);
+            
+            Console.WriteLine("Int: " + myNum1);
+            Console.WriteLine("Double: " + myNum2);
+
+            int valor0 = PlusMethod(10, 10);
+            Console.WriteLine(valor0); 
+            
+            double valor1 = PlusMethod(10, 10); 
+            Console.WriteLine(valor1);
+            
+            // Al usar la sobrecarga debe pasarse como valor de  parámetro
+            // un valor que represente el tipo de dato requerido.
+            string valor2 = PlusMethod(10.0, 10);
+            Console.WriteLine(valor2);
+
+        }
+        
+        // -------------------------------------------------------------
+        static int PlusMethod(int x, int y) {
+            return x + y;
+        }
+
+        // -------------------------------------------------------------
+        static double PlusMethod(double x, double y) {
+            return x + y;
+        }
+
+        // -------------------------------------------------------------
+        static string PlusMethod(double x, int y) {
+            return $"El valor de la suma entre {x} y {y} es: {x+y}";
+        }
+        // -------------------------------------------------------------
+    }
+}
